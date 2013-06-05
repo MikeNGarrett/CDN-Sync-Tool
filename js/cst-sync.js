ee6a42251a8452167bbe488ff8484d45a2d4cb0a
MikeNGarrett <mgarrett@webdevelopmentgroup.com>
2013-06-05 15:00:48 -0400

    Cookies and breakout of functionality

--- js/cst-sync.js
+++ js/cst-sync.js
@@ -4,6 +4,27 @@ jQuery(document).ready(function($) {
 		action: 'cst_get_queue',
 		cst_check: syncAjax.cst_check
 	};
+
+function sync() {
+	console.log("The ID for index " + x + " is: " + queue[x].id);
+	var passedFile = queue[x]; // eventually this will be populated by pulling element from cookie-based queue
+	var syncFileData = {
+		action: 'cst_sync_file',
+		cst_check: syncAjax.cst_check,
+		file: passedFile
+	};
+	$.ajax({
+		type: "post",
+		url: syncAjax.ajax_url,
+		data: syncFileData,
+		success: function(response) {
+			$(".cst-progress").append(response.message);
+			console.log(document.cookie.cst_queue);
+			sync();
+		}
+	});
+}
+
 	// We can also pass the url value separately from ajaxurl for front end AJAX implementations
 	$.ajax({
 		type: "post",
@@ -11,26 +32,20 @@ jQuery(document).ready(function($) {
 		data: data,
 		success: function(response) {
 			var queue = response.slice(0,5);
-			alert("Number of items: " + queue.length);
+			var date = new Date();
+			date.setTime(date.getTime()+(10*24*60*60*1000));
+			var expires = date.toGMTString();
+			document.cookie =
+  'cst_queue='+queue='; expires='+expires+'; path=/';
+			console.log("Number of items: " + queue.length);
 
 			if (queue.length > 0) {
-				for (var x = 0; x < queue.length; x++) {
-					alert("The ID for index " + x + " is: " + queue[x].id);
-					var passedFile = queue[x]; // eventually this will be populated by pulling element from cookie-based queue
-					var syncFileData = {
-						action: 'cst_sync_file',
-						cst_check: syncAjax.cst_check,
-						file: passedFile
-					};
-					$.ajax({
-						type: "post",
-						url: syncAjax.ajax_url,
-						data: syncFileData,
-						success: function(response) {
-							$(".cst-progress").append(response);
-						}
-					});
-				}
+			//				for (var x = 0; x < queue.length; x++) {
+				console.log(queue);
+				console.log(document.cookie.cst_queue);
+				//sync(queue[queue.length - 1]);
+			//				}
+
 			}
 
 			// Upon completion, show the Return to Options Page button
@@ -49,7 +64,7 @@ jQuery(document).ready(function($) {
 			// 		data: syncFileData,
 			// 		success: function(response) {
 			// 			i++;
-			// 			alert('The value of i is: ' + i);
+			// 			console.log('The value of i is: ' + i);
 			// 			//if (i > 3) {
 			// 				return;
 			// 			// }
