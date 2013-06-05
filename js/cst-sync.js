179ac53fad0a225764bba8b1717f00dcb7c73c0f
MikeNGarrett <mgarrett@webdevelopmentgroup.com>
2013-06-05 15:36:45 -0400

    cookies + json

--- js/cst-sync.js
+++ js/cst-sync.js
@@ -43,8 +43,7 @@ function sync() {
 
 			if (queue.length > 0) {
 			//				for (var x = 0; x < queue.length; x++) {
-				console.log(queue);
-				console.log(document.cookie.cstQueue);
+				console.log(readCookie("cstQueue"));
 				//sync(queue[queue.length - 1]);
 			//				}
 
@@ -82,3 +81,13 @@ function sync() {
 	});
 
 });
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
